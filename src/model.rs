295a4d6579f89759e05cae4dcc623754123fdca1
Luis Ayuso <luis.ayuso@bhs-technologies.com>
2023-09-19 18:00:34 +0200

    tooling for the solver

--- src/model.rs
+++ src/model.rs
@@ -1,9 +1,18 @@
 use crate::skills::Skill;
 
-#[derive(PartialEq, Eq, Debug, Clone)]
+#[derive(PartialEq, Eq, Debug, Clone, Copy)]
 pub enum Team {
     Defender,
     Attacker,
+}
+
+impl Team {
+    pub fn opposite(&self) -> Self {
+        match self {
+            Self::Attacker => Self::Defender,
+            Self::Defender => Self::Attacker,
+        }
+    }
 }
 
 #[derive(PartialEq, Eq, Debug, Clone)]
@@ -46,8 +55,18 @@ impl Player {
             skills: s,
         }
     }
+
     pub fn ident(&self) -> char {
         self.ident
+    }
+    pub fn strength(&self) -> u8 {
+        self.strength
+    }
+    pub fn team(&self) -> Team {
+        self.team
+    }
+    pub fn skills(&self) -> &[Skill] {
+        &self.skills
     }
 }
 
@@ -72,41 +91,68 @@ impl Zone {
     }
 }
 pub const LineWidth: usize = 15;
-pub const HalfFieldLenght: usize = 13;
+pub const HalfFieldLength: usize = 13;
 
-pub type Coords = (usize, usize);
+#[derive(PartialEq, Eq, Debug, Clone, Copy)]
+pub struct Coords(usize, usize);
+
+impl Coords {
+    pub fn new(i: usize, j: usize) -> Coords {
+        Coords(i, j)
+    }
+    pub fn i(&self) -> usize {
+        self.0
+    }
+    pub fn j(&self) -> usize {
+        self.1
+    }
+}
+
+impl std::ops::Add<Coords> for Coords {
+    type Output = Coords;
+    fn add(self, rhs: Coords) -> Self::Output {
+        Coords(self.0 + rhs.0, self.1 + rhs.1)
+    }
+}
+impl std::ops::Add<(isize, isize)> for Coords {
+    type Output = Coords;
+    fn add(self, rhs: (isize, isize)) -> Self::Output {
+        match (
+            self.0.checked_add_signed(rhs.0),
+            self.1.checked_add_signed(rhs.1),
+        ) {
+            (None, None) | (None, Some(_)) | (Some(_), None) => self,
+            (Some(a), Some(b)) => Coords(a, b),
+        }
+    }
+}
 
 pub type Line = [Zone; LineWidth];
-pub struct HalfField([Line; HalfFieldLenght]);
+pub struct HalfField {
+    lines: [Line; HalfFieldLength],
+    out: Zone,
+}
 
 impl HalfField {
-    pub fn new(lines: [Line; HalfFieldLenght]) -> Self {
-        HalfField(lines)
+    pub fn new(lines: [Line; HalfFieldLength]) -> Self {
+        HalfField {
+            lines,
+            out: Zone::Out,
+        }
     }
 
     pub fn get_line(&self, i: usize) -> &Line {
-        &self.0[i]
+        &self.lines[i]
     }
     pub fn get_zone(&self, coords: &Coords) -> &Zone {
-        &self.0[coords.0][coords.1]
-    }
-
-    /**
-     * 0,1,2
-     * 3,x,4
-     * 5,6,7
-     */
-    pub fn get_neighbours(&self, coords: &Coords) -> [Zone; 8] {
-        [
-            Zone::Out,
-            Zone::Out,
-            Zone::Out,
-            Zone::Out,
-            Zone::Out,
-            Zone::Out,
-            Zone::Out,
-            Zone::Out,
-        ]
+        if coords.0 >= HalfFieldLength {
+            &self.out
+        } else if coords.1 >= LineWidth {
+            &self.out
+        } else {
+            println!("{:?} {} {} ", coords, HalfFieldLength, LineWidth);
+            &self.lines[coords.0][coords.1]
+        }
     }
 }
 
@@ -124,7 +170,7 @@ impl std::fmt::Display for Zone {
 impl std::fmt::Display for HalfField {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "|===============|\n")?;
-        for l in &self.0 {
+        for l in &self.lines {
             write!(f, "|")?;
             for z in l {
                 write!(f, "{}", z)?;
--- src/solver.rs
+++ src/solver.rs
@@ -1,6 +1,6 @@
-use crate::model::{Coords, HalfField, HalfFieldLenght, LineWidth, Player, Zone};
+use crate::model::{Coords, HalfField, HalfFieldLength, LineWidth, Player, Team, Zone};
 
-const ScrimmageLine: usize = HalfFieldLenght - 1;
+const SCRIMMAGE_LINE: usize = HalfFieldLength - 1;
 
 #[derive(Debug)]
 struct Solution;
@@ -11,23 +11,91 @@ impl Solution {
     }
 }
 
-fn block_odds(attacker: (Player, &Coords), victim_coords: &Coords, hf: &HalfField) -> f32 {
-    let victim = match hf.get_zone(victim_coords) {
-        Zone::Empty => panic!("block target not a player"),
+fn get_neighbour_coords(center: &Coords) -> [Coords; 8] {
+    [
+        *center + (-1 as isize, -1 as isize),
+        *center + (0 as isize, -1 as isize),
+        *center + (1 as isize, -1 as isize),
+        *center + (-1 as isize, 0 as isize),
+        *center + (1 as isize, 0 as isize),
+        *center + (-1 as isize, 1 as isize),
+        *center + (0 as isize, 1 as isize),
+        *center + (1 as isize, 1 as isize),
+    ]
+}
+
+fn is_tackled(player: (&Coords, &Player), hf: &HalfField) -> bool {
+    get_neighbours_of_team(player.0, player.1.team().opposite(), hf)
+        .next()
+        .is_some()
+}
+
+/**
+ * 0,1,2
+ * 3,x,4
+ * 5,6,7
+ */
+fn get_neighbours_of_team<'a>(
+    center: &Coords,
+    team: Team,
+    hf: &'a HalfField,
+) -> impl std::iter::Iterator<Item = (Coords, &'a Player)> {
+    get_neighbour_coords(center)
+        .into_iter()
+        .map(|coords| (coords, hf.get_zone(&coords)))
+        .filter_map(|(coords, zone)| match zone {
+            Zone::Out | Zone::Empty => None,
+            Zone::Player(player) => Some((coords, player)),
+        })
+        .filter(move |(_, player)| player.team() == team)
+}
+
+fn block_odds(attacker: (&Coords, &Player), defender_coords: &Coords, hf: &HalfField) -> f32 {
+    let (attacker_coords, attacker) = attacker;
+
+    let defender = match hf.get_zone(defender_coords) {
+        Zone::Out | Zone::Empty => panic!("block target not a player"),
         Zone::Player(p) => p,
     };
 
-    // get players tackling attacker
-    
+    let att_stg = attacker.strength() + get_supports(defender_coords, attacker.team(), hf) as u8;
+    let def_stg = defender.strength() + get_supports(attacker_coords, defender.team(), hf) as u8;
 
     0.0
+}
+
+#[derive(PartialEq, Eq, Debug, Clone, Copy)]
+enum Dice {
+    ThreeAgainst,
+    TwoAgainst,
+    One,
+    Two,
+    Three,
+}
+
+fn get_dice(att_stg: u8, def_stg: u8) -> Dice {
+    match (att_stg, def_stg) {
+        (a, b) if a == b => Dice::One,
+        (a, b) if b > a * 2 => Dice::ThreeAgainst,
+        (a, b) if b > a => Dice::TwoAgainst,
+        (a, b) if a > b * 2 => Dice::Three,
+        (a, b) if a > b => Dice::Two,
+        (_, _) => unreachable!(),
+    }
+}
+
+fn get_supports(pos: &Coords, team: Team, hf: &HalfField) -> usize {
+    get_neighbours_of_team(pos, team, hf)
+        .filter(|(coords, player)| !is_tackled((&coords, player), hf))
+        .count()
 }
 
 /// xxOxx
 /// xPBxP
 /// xxxPx
 fn play_1(hf: &HalfField, center: &Coords) -> Solution {
-    let (i, j) = center;
+    let i = center.i();
+    let j = center.j();
     let first_blitz = (i, j - 1);
     let second_block = (i + 1, j + 2);
     let third_block = (i + 2, j + 1);
@@ -38,17 +106,17 @@ fn play_1(hf: &HalfField, center: &Coords) -> Solution {
 
 fn solve_for_play_1(hf: &HalfField, los: &[Coords]) -> Vec<Solution> {
     los.iter()
-        .filter(|(_, j)| j >= &2 && j + 2 < LineWidth)
+        .filter(|c| c.j() >= 2 && c.j() + 2 < LineWidth)
         .map(|c| play_1(hf, c))
         .collect()
 }
 
 fn get_los(hf: &HalfField) -> Vec<Coords> {
-    hf.get_line(ScrimmageLine)
+    hf.get_line(SCRIMMAGE_LINE)
         .iter()
         .enumerate()
         .filter(|(_, z)| z.is_player())
-        .map(|(j, _)| (ScrimmageLine, j))
+        .map(|(j, _)| Coords::new(SCRIMMAGE_LINE, j))
         .collect()
 }
 
@@ -61,15 +129,56 @@ pub fn solve(hf: &HalfField) {
 
 #[cfg(test)]
 mod tests {
-
-    use crate::parser::from_str;
-
     use super::*;
+    use crate::{model::Team::Attacker, parser::from_str};
 
     #[test]
     fn play_1() {
         let one = include_str!("../demos/one.hf");
         let hf = from_str(one).expect("past gut");
         let los = get_los(&hf);
+        assert_eq!(los.len(), 3);
+    }
+    #[test]
+    fn dices() {
+        for i in 1..6 {
+            assert_eq!(get_dice(i, i), Dice::One);
+        }
+        for i in 1..6 {
+            assert_eq!(get_dice(i, i + 1), Dice::TwoAgainst);
+            assert_eq!(get_dice(i + 1, i), Dice::Two);
+        }
+        for i in 1..6 {
+            assert_eq!(get_dice(i, i * 2), Dice::TwoAgainst);
+            assert_eq!(get_dice(i * 2, i), Dice::Two);
+        }
+        for i in 1..6 {
+            assert_eq!(get_dice(i, i * 2 + 1), Dice::ThreeAgainst);
+            assert_eq!(get_dice(i * 2 + 1, i), Dice::Three);
+        }
+    }
+
+    #[test]
+    fn block() {
+        let one = include_str!("../demos/two.hf");
+        let hf = from_str(one).expect("past gut");
+
+        let attacker = Player::new('1').with_team(Attacker);
+        let in_front_of = Coords::new(SCRIMMAGE_LINE + 1, 6);
+        let left_of = Coords::new(SCRIMMAGE_LINE + 1, 6 - 1);
+        let right_of = Coords::new(SCRIMMAGE_LINE + 1, 6 + 1);
+
+        let free = Coords::new(SCRIMMAGE_LINE + 2, 6 + 1);
+
+        assert!(is_tackled((&in_front_of, &attacker), &hf));
+        assert!(is_tackled((&left_of, &attacker), &hf));
+        assert!(is_tackled((&right_of, &attacker), &hf));
+        assert!(!is_tackled((&free, &attacker), &hf));
+
+        block_odds(
+            (&in_front_of, &attacker),
+            &Coords::new(SCRIMMAGE_LINE, 6),
+            &hf,
+        );
     }
 }
